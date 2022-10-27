3e947f2b2ac81a885137bc616db80ff844de88f5
Luis <luiscelestre74@gmail.com>
2022-10-27 20:09:53 -0300

    commit n

--- gen_contraseña.py
+++ gen_contraseña.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+import key_gen
 
 class App:
     ANCHO = 300
@@ -8,10 +8,33 @@ class App:
     def __init__(self):
 
         self.root = tk.Tk()
-        self.root.geometry()
+        self.root.geometry(f"{App.ANCHO}x{App.ALTO}")
         self.root.title("Generador de contraseña")
 
+        self.texto1 = tk.Label(text="Cantidad de caracteres")
+        self.texto1.place(x=20,y=20)
+
+        self.text_input = tk.Entry(width=2)
+        self.text_input.place(x=160,y=20)
+
+        self.cantidad = tk.IntVar
+
+        self.text_key = tk.Label(textvariable= self.cantidad)
+        self.text_key.place(x = 20,y=40)
+
+        self.boton_gen = tk.Button(text="Generar clave", command = self.generar_contraseña )
+        self.boton_gen.place(x = 20,y=50)
+
         self.root.mainloop()
+
+    def generar_contraseña(self):
+
+        cant = int(self.text_input.get())
+      
+        contraseña_generada = key_gen.gen_password(cant)
+        self.cantidad.set(contraseña_generada)
+        
+       
 
 def main():
     v= App()
