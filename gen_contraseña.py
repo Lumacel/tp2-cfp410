2495bb88f19c36a8e4cc9a7da8a9cb07c6d44494
Lumacel <luiscelestre74@gmail.com>
2022-10-28 06:45:26 -0300

    ej2 terminado

--- gen_contraseña.py
+++ gen_contraseña.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 import key_gen
 
+MAX_LENGHT=20
+
 class App:
     ANCHO = 300
     ALTO = 200
+    MAX_LENGHT=40
 
     def __init__(self):
 
@@ -11,31 +14,58 @@ class App:
         self.root.geometry(f"{App.ANCHO}x{App.ALTO}")
         self.root.title("Generador de contraseña")
 
-        self.texto1 = tk.Label(text="Cantidad de caracteres")
+        self.texto1 = tk.Label(text=f"Longitud contraseña (MAX {App.MAX_LENGHT}):")
         self.texto1.place(x=20,y=20)
 
-        self.text_input = tk.Entry(width=2)
-        self.text_input.place(x=160,y=20)
-
-        self.cantidad = tk.IntVar
-
-        self.text_key = tk.Label(textvariable= self.cantidad)
-        self.text_key.place(x = 20,y=40)
+        self.text_input = tk.Entry(width=5)
+        self.text_input.place(x=200,y=20)
+        self.text_input.focus_set()
+        
+        self.password = tk.StringVar()
+        self.notas = tk.StringVar()
 
         self.boton_gen = tk.Button(text="Generar clave", command = self.generar_contraseña )
         self.boton_gen.place(x = 20,y=50)
 
+        self.text_key = tk.Label(textvariable= self.password)
+        self.text_key.place(x = 20,y=110)
+
+        self.notific = tk.Label(textvariable= self.notas)
+        self.notific.place(x= 20, y = 170)
+
+
+        self.root.bind('<Return>', self.generar_contraseña)
+
         self.root.mainloop()
 
-    def generar_contraseña(self):
+    def clip(self, text):
+            '''
+            Copia el parámetro enviado 'text' al portapapeles
+            '''
+            self.root.clipboard_clear()
+            self.root.clipboard_append(text)
 
-        cant = int(self.text_input.get())
-      
-        contraseña_generada = key_gen.gen_password(cant)
-        self.cantidad.set(contraseña_generada)
+    def generar_contraseña(self,*args):
+
+        try:
+            cant = abs(int(self.text_input.get()))
+            if cant <= App.MAX_LENGHT:
+                contraseña_generada = key_gen.gen_password(cant)
+               
+            else:
+                contraseña_generada = key_gen.gen_password(App.MAX_LENGHT)
+
+            self.password.set(contraseña_generada)
+            self.clip(contraseña_generada)
+            self.notas.set("Contraseña copiada al portapapeles")
+            
+        except:
+            if self.text_input.get() == "":
+                self.notas.set("ERROR!! ingrese algún valor")
+            else:
+                self.notas.set("ERROR!! ingrese valor numérico")
+
         
-       
-
 def main():
     v= App()
 
